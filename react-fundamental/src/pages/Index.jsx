d9d9a714ddc9b8624cb50c48d5c19ab49ef350a8
ajayfaul <muhammadfajarm@gmail.com>
2025-03-20 12:14:19 +0700

    Fitur filter data dan pengenalan useState
    feat: Add search functionality to homepage for filtering articles

--- react-fundamental/src/pages/Index.jsx
+++ react-fundamental/src/pages/Index.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import posts from "../posts.json";
 import Article from "../components/Article";
 
 function Homepage() {
+  //   let search = "";
+  const [search, setSearch] = useState("");
+  const changeSearch = (event) => {
+    // console.log(event.target.value);
+    // search = event.target.value;
+    setSearch(event.target.value);
+  };
+
   return (
     <>
       <h1>Simple Blog</h1>
-      {posts.map((blog) => (
-        <Article title={blog.title} tags={blog.tags} date={blog.date} />
+      <div>
+        Cari Artikel: <input onChange={changeSearch}></input>{" "}
+      </div>
+      <small>Ditemukan 0 data dengan pencarian kata {search}</small>
+      {/* kita destructure title, tags, dan date */}
+      {posts.map(({ title, tags, date }, index) => (
+        // <Article title={title} tags={tags} date={date} />
+        // menggunakan spread operator
+        <Article {...{ title, tags, date }} key={index} />
       ))}
     </>
   );
