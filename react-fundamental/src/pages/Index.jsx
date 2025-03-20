8fc0e75430a165df2aef30273a6349b4c3df52a0
ajayfaul <muhammadfajarm@gmail.com>
2025-03-20 14:51:41 +0700

    feat: Add total posts count to Search component for improved user feedback

--- react-fundamental/src/pages/Index.jsx
+++ react-fundamental/src/pages/Index.jsx
@@ -4,16 +4,20 @@ import Article from "../components/Article";
 import Search from "../components/Search";
 function Homepage() {
   const [posts, setPosts] = useState(postsData);
+  const [totalPosts, setTotalPosts] = useState(0);
+
   const onSearchChange = (value) => {
     const filteredPosts = postsData.filter((item) =>
       item.title.toLowerCase().includes(value.toLowerCase())
     );
     setPosts(filteredPosts);
+
+    setTotalPosts(filteredPosts.length);
   };
   return (
     <>
       <h1>Simple Blog</h1>
-      <Search onSearchChange={onSearchChange} />
+      <Search onSearchChange={onSearchChange} totalPosts={totalPosts} />
       {/* kita destructure title, tags, dan date */}
 
       {/* diperpendek lagi */}
