9ef90bef8753499cff2323edee1c243c2c220677
ajayfaul <muhammadfajarm@gmail.com>
2025-03-20 15:04:47 +0700

    conditional rendering 2
    feat: Implement conditional rendering for new articles in Article component

--- react-fundamental/src/App.jsx
+++ react-fundamental/src/App.jsx
@@ -3,12 +3,8 @@ import "./App.css";
 import Home from "./pages/Index";
 
 function App() {
-  const value = true;
-
   return (
     <>
-      {/* conditional rendering */}
-      {value ? <h1>True</h1> : <h1>False</h1>}
       <Home />
       {/* <Article name="beruk" titles={["Tutorial react", "React Fundamental"]} />
       <br />
--- react-fundamental/src/components/Article.jsx
+++ react-fundamental/src/components/Article.jsx
@@ -1,9 +1,21 @@
+const ArticleStatus = ({ isNew }) => {
+  return isNew && <span>-- Baru</span>;
+};
+
+const NewArticle = () => {
+  return <span>-- Baru</span>;
+};
+
 function Article(props) {
   return (
     <>
       <h3>{props.title}</h3>
       <small>
-        Date: {props.date}, tags: {props.tags.join(", ")}
+        Date: {props.date}, tags: {props.tags.join(", ")}{" "}
+        {/* {props.isNew ? "--> New" : "lama"} */}
+        {/*  cara singkat conditional rendering */}
+        <ArticleStatus isNew={props.isNew} />
+        {props.isNew && NewArticle()}
       </small>
     </>
   );
