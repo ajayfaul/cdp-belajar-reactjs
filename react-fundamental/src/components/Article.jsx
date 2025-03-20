0a055bcc660013e988c2d967c892e8898118fcd6
ajayfaul <muhammadfajarm@gmail.com>
2025-03-20 10:41:44 +0700

    feat: Update Article component to display titles and user name
    Pengenalan JSX dan mereturn sebuah element statis maupun dinamis, juga membungkus elemen dengan div

--- react-fundamental/src/components/Article.jsx
+++ react-fundamental/src/components/Article.jsx
@@ -1,10 +1,56 @@
 function Article() {
+  const name = "John Doe";
+  const titles = [
+    "Belajar React itu menyenangkan",
+    "Tutorial React dari Nol hingga Mahir",
+    "React is Awesome",
+  ];
+
+  //   title.map(item => item);
+
   return (
-    <div>
-      Ini adalah component pertama
-      <h1>Article</h1>
-      <p>Ini adalah contentnya</p>
-    </div>
+    // memanfaatkan react fragment untuk membungkus beberapa element
+    <>
+      <div>
+        Ini adalah component pertama
+        <h1>Article</h1>
+        // kalau satu element bisa seperti Ini
+        {/* <h2>
+          {titles.map((title) => (
+            <div>{title}</div>
+          ))}
+        </h2> */}
+        // Contoh multiple 1
+        <h2>
+          {titles.map((title) => {
+            return <div>{title}</div>;
+          })}
+        </h2>
+        // Contoh multiple 3
+        <h2>
+          {titles.map((title) => {
+            return (
+              <>
+                <div>{title}</div>
+                <div>{title}</div>
+                <div>{title}</div>
+              </>
+            );
+          })}
+        </h2>
+        <p>Halo, nama saya adalah {name}</p>
+        {/* <p>Halo, nama saya adalah {name}</p> */}
+      </div>
+    </>
+    // membungkus beberapa element dengan div
+    // <div>
+    //   <div>
+    //     Ini adalah component pertama
+    //     <h1>Article</h1>
+    //     <p>Halo, nama saya adalah {name}</p>
+    //     <p>Halo, nama saya adalah {name}</p>
+    //   </div>
+    // </div>
   );
 }
 
