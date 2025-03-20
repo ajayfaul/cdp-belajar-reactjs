04b63f8e593cca21449a4ba2ed6d286828e74d4b
ajayfaul <muhammadfajarm@gmail.com>
2025-03-20 10:21:14 +0700

    feat: Pengenalan Component dasar

---
+++ react-fundamental/src/components/Article.jsx
@@ -0,0 +1,11 @@
+function Article() {
+  return (
+    <div>
+      Ini adalah component pertama
+      <h1>Article</h1>
+      <p>Ini adalah contentnya</p>
+    </div>
+  );
+}
+
+export default Article;
