deed09be315bcf6f1b87b83e5dae4d14a602dc8c
ajayfaul <muhammadfajarm@gmail.com>
2025-03-20 17:09:13 +0700

    onClick Methods
    feat: Enhance Search component with Enter key support and button functionality

--- react-fundamental/src/components/Search.jsx
+++ react-fundamental/src/components/Search.jsx
@@ -2,15 +2,30 @@ import { useState } from "react";
 function Search(props) {
   const [search, setSearch] = useState("");
 
-  const onSearchChange = (event) => {
-    setSearch(event.target.value);
-    props.onSearchChange(event.target.value);
+  const onSearchChange = () => {
+    // setSearch(event.target.value);
+    props.onSearchChange(search);
+  };
+
+  //   const handleClick = () => {
+  //     console.log("test");
+  //   };
+
+  const searchKeyDown = (event) => {
+    if (event.key === "Enter") {
+      onSearchChange();
+    }
   };
 
   return (
     <>
       <div>
-        Cari Artikel: <input onChange={onSearchChange}></input>{" "}
+        Cari Artikel:{" "}
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={searchKeyDown}
+        ></input>{" "}
+        <button onClick={onSearchChange}>Search</button>
       </div>
       <small>
         Ditemukan {props.totalPosts} data dengan pencarian kata {search}
