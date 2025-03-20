bc98eec4a50798bb99fc043d7fcf770c55058c16
ajayfaul <muhammadfajarm@gmail.com>
2025-03-20 14:41:00 +0700

    feat: Add Search component and integrate it into the homepage for article filtering
    Mengembalikan value ke parent melalui props

--- react-fundamental/src/pages/Index.jsx
+++ react-fundamental/src/pages/Index.jsx
@@ -1,28 +1,26 @@
 import { useState } from "react";
-import posts from "../posts.json";
+import postsData from "../posts.json";
 import Article from "../components/Article";
-
+import Search from "../components/Search";
 function Homepage() {
-  //   let search = "";
-  const [search, setSearch] = useState("");
-  const changeSearch = (event) => {
-    // console.log(event.target.value);
-    // search = event.target.value;
-    setSearch(event.target.value);
+  const [posts, setPosts] = useState(postsData);
+  const onSearchChange = (value) => {
+    const filteredPosts = postsData.filter((item) =>
+      item.title.toLowerCase().includes(value.toLowerCase())
+    );
+    setPosts(filteredPosts);
   };
-
   return (
     <>
       <h1>Simple Blog</h1>
-      <div>
-        Cari Artikel: <input onChange={changeSearch}></input>{" "}
-      </div>
-      <small>Ditemukan 0 data dengan pencarian kata {search}</small>
+      <Search onSearchChange={onSearchChange} />
       {/* kita destructure title, tags, dan date */}
-      {posts.map(({ title, tags, date }, index) => (
+
+      {/* diperpendek lagi */}
+      {posts.map((props, index) => (
         // <Article title={title} tags={tags} date={date} />
         // menggunakan spread operator
-        <Article {...{ title, tags, date }} key={index} />
+        <Article {...props} key={index} />
       ))}
     </>
   );
